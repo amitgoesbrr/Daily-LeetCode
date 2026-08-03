1d798aae0c29f8f77dee3f1c77f219000c2da6bc
Amit Das <234362796+amitgoesbrr@users.noreply.github.com>
2026-08-03 22:12:27 +0530

    Time: 15 ms (40.85%), Space: 24 MB (9.01%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            key = [0] * 26
+            for c in s:
+                key[ord(c) - 97] += 1
+            res[tuple(key)].append(s)
+        return list(res.values())
